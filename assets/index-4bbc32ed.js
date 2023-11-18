0df796f095e7be4b2f10e306cb433b12e6af339c
Maxim Alekseev <112779335+Alemy75@users.noreply.github.com>
2023-11-18 21:31:19 +0300

    Updates

--- assets/index-4bbc32ed.js
+++
@@ -1,6 +0,0 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const c=new Date("2023-12-22T14:00:00").getTime(),a=setInterval(l,1e3);function l(){const i=new Date().getTime(),r=c-i,s=Math.floor(r/(1e3*60*60*24)),o=Math.floor(r%(1e3*60*60*24)/(1e3*60*60)),e=Math.floor(r%(1e3*60*60)/(1e3*60)),t=Math.floor(r%(1e3*60)/1e3),n=document.getElementById("timer");n.innerHTML=`
-    <div><span>${s}</span> дней</div>
-    <div><span>${o}</span> часов</div>
-    <div><span>${e}</span> минут</div>
-    <div><span>${t}</span> секунд</div>
-  `,r<0&&(clearInterval(a),n.innerHTML="<h2>Таймер истек!</h2>")}
